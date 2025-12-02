bfc765794d14b7d43548e6c1c11062f0d0e56eba
bogdan-kirichenko <bogdanlirichenko57@gmail.com>
2025-12-02 23:51:30 +0200

    Solved 26 Remove duplicates in sorted Array

--- Easy/TwoSum.py
+++ Easy/TwoSum.py
@@ -1,5 +1,12 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        d1 = {}
+        for i, num in enumerate(nums):
+            difference = target - num
+            if difference in d1:
+                return [d1[difference], i]
+            else:
+                d1[num] = i
         d1 = {}
         for i, num in enumerate(nums):
             difference = target - num
